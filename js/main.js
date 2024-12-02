233755a78b12a5819373034671e5a0e4fde824a8
GenixE <106990876+GenixE@users.noreply.github.com>
2024-12-02 15:55:12 +0100

    some changes

--- js/main.js
+++ js/main.js
@@ -57,16 +57,20 @@
     { offset: "80%" }
   );
 
-  // Portfolio isotope and filter
-  var portfolioIsotope = $(".portfolio-container").isotope({
-    itemSelector: ".portfolio-item",
-    layoutMode: "fitRows",
-  });
-  $("#portfolio-flters li").on("click", function () {
-    $("#portfolio-flters li").removeClass("active");
-    $(this).addClass("active");
-
-    portfolioIsotope.isotope({ filter: $(this).data("filter") });
+  $(document).ready(function () {
+    // Portfolio isotope and filter
+    var portfolioIsotope = $(".portfolio-container").isotope({
+      itemSelector: ".portfolio-item",
+      layoutMode: "fitRows",
+    });
+  
+    $("#portfolio-flters li").on("click", function () {
+      $("#portfolio-flters li").removeClass("active");
+      $(this).addClass("active");
+  
+      var filterValue = $(this).data("filter");
+      portfolioIsotope.isotope({ filter: filterValue });
+    });
   });
 
   // Back to top button
